2e0e37ddb6f1f6d4f1605b3a8a1f34b1eaf802a7
Ali Firozmand <firozmand.dev@gmail.com>
2023-06-13 16:56:16 +0330

    first

---
+++ src/App.vue
@@ -0,0 +1,77 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template >
+  <div class="  flex w-screen  bg-red-500 p-2 ">
+    <div class=" p-2 mx-5"> <RouterLink  to="/">Home</RouterLink> </div>
+    <div class=" p-2 mx-5"> <RouterLink is="div" class=" p-2 mx-5" to="/login">login</RouterLink> </div>
+
+  </div>
+
+  <RouterView />
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+  max-height: 100vh;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+nav {
+  width: 100%;
+  font-size: 12px;
+  text-align: center;
+  margin-top: 2rem;
+}
+
+nav a.router-link-exact-active {
+  color: var(--color-text);
+}
+
+nav a.router-link-exact-active:hover {
+  background-color: transparent;
+}
+
+nav a {
+  display: inline-block;
+  padding: 0 1rem;
+  border-left: 1px solid var(--color-border);
+}
+
+nav a:first-of-type {
+  border: 0;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+
+  nav {
+    text-align: left;
+    margin-left: -1rem;
+    font-size: 1rem;
+
+    padding: 1rem 0;
+    margin-top: 1rem;
+  }
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,8 @@
+<script setup>
+</script>
+
+<template class="overflow-hidden" >
+  <main class=" flex w-screen h-screen bg-black text-white font-bold justify-center items-center">
+     nader
+  </main>
+</template>
